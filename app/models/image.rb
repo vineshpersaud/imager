26c825803e3d33bf2a2b257c2bbdaeaf84ce875c
github username <github email address>
2019-02-20 18:49:47 -0500

    setup image to upload via paperclip gem

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ApplicationRecord
+  has_attached_file :image ,styles:{large: "600x600>" , medium: "300x300>",thumb: "150x150#"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   has_many :comments
   belongs_to :user
 end
