b773db5f40c9f2648cf31f421b2c046e010c1a36
github username <github email address>
2019-03-06 16:17:58 -0500

    add style and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,37 @@ import './App.css';
 import UserForm from './containers/UserForm.js'
 import ImageForm from './containers/ImageForm.js'
 import Images from './containers/Images.js'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import NavBar from './components/NavBar';
 
-class App extends Component {
-  render() {
-    return (
+// class App extends Component {
+//   render() {
+//     return (
+//
+//       <div className="App">
+//         <ImageForm/>
+//         <Images/>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
 
-      <div className="App">
-        <ImageForm/>
-        <Images/>
+
+const App = (props) => {
+  return (
+    <Router>
+      <div className="app">
+        <NavBar />
+        <Route exact path="/images" component={Images} />
+        <Route exact path="/images/new" component={ImageForm} />
       </div>
-    );
-  }
-}
+    </Router>
+  );
+};
 
-export default App;
+export default App
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+       <NavLink to="/" exact>Home</NavLink>
+       <NavLink to="/images" exact>Images</NavLink>
+       <NavLink to="/images/new" exact>New Image</NavLink>
+    </div>
+  );
+};
+
+export default NavBar;
