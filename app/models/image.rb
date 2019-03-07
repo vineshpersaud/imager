1a16229818456e0e9828aa9e4ba4528c06302626
github username <github email address>
2019-03-07 16:05:57 -0500

    add username to comments table

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,8 @@
 class Image < ApplicationRecord
   has_one_attached :picture
   has_many :comments
-  belongs_to :user, optional: true
   include Rails.application.routes.url_helpers
-
+  
   def url
      rails_blob_path(self.picture)
   end
